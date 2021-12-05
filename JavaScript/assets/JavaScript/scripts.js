9d65775050bcc48261817cc99e3908548489d460
Jonas Felix <jonas.medeiros.felix@gmail.com>
2021-12-05 16:32:05 -0300

    i create a counter with addEventListener

--- JavaScript/assets/JavaScript/scripts.js
+++ JavaScript/assets/JavaScript/scripts.js
@@ -1,12 +1,18 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
-function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+function increment() {  
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
 }
 
 function decrement() {
     currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+   currentNumberWrapper.innerHTML = currentNumber;
 }
+
+add.addEventListener("click", increment);
+decrease.addEventListener("click", decrement);
+
+
+
