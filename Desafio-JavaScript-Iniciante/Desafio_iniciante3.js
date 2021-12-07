af7e37b872d5e4f2f2f63048913e010d103defc0
Jonas Felix <jonas.medeiros.felix@gmail.com>
2021-12-07 11:14:17 -0300

    when X =1

--- Desafio-JavaScript-Iniciante/Desafio_iniciante3.js
+++ Desafio-JavaScript-Iniciante/Desafio_iniciante3.js
@@ -2,19 +2,20 @@
 Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
 */
 
-
 function impar(){
-    let X = parseInt(gets());
-    if( X >=1 && X <= 1000){
-      var impar = 0
-      
-      for ( let i = 0 ; (impar + 2) <= X; i++ ) {
-        impar = 2 * i + 1;
-        console.log(impar);
-      }
-    }else{
-      console.log("insira um inteiro de 1 a 1000:")
-    }
+  let X = parseInt(gets());
+  if( X >=1 && X <= 1000){
+    let impar = 0;
+    let i = 0;
+    
+    do{
+      impar = 2 * i + 1;
+      console.log(impar);
+      i++;
+    } while ((impar +2) <= X );
+  }else{
+    console.log("insira um inteiro de 1 a 1000:")
+  }
 }
 
 impar();
